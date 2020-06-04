c62ea7cababd08b4cc72dcb5e783bbb6eec5fc3f
korr237 <korr237i@yandex.ru>
2020-06-04 22:12:52 +0300

    Moved work with ui to signals-slots.

--- host/source/watcher.py
+++ host/source/watcher.py
@@ -8,30 +8,8 @@ from lib.controls import Controller
 
 # Main thread for getting/throwing data from/to MBee module and for checking all's OK
 class WatcherThread(QThread):
-    coordinate_value_sig = pyqtSignal(float)
     road_battery_sig = pyqtSignal(float)
-
-    def rssi_value_slot(self, value):
-        if value is None:
-            global_.window.ui.RSSI.setStyleSheet("background-color: gray; color: black")
-            # styleChangeProperty(global_.window.ui.RSSI, 'color', 'black')
-            # styleChangeProperty(global_.window.ui.RSSI, 'background-color', 'gray')
-        elif (value < -90):
-            global_.window.ui.RSSI.setStyleSheet("background-color: black; color: white")
-            # styleChangeProperty(global_.window.ui.RSSI, 'color', 'white')
-            # styleChangeProperty(global_.window.ui.RSSI, 'background-color', 'black')
-        elif (value < -80):
-            global_.window.ui.RSSI.setStyleSheet("background-color: red; color: black")
-            # styleChangeProperty(global_.window.ui.RSSI, 'color', 'black')
-            # styleChangeProperty(global_.window.ui.RSSI, 'background-color', 'red')
-        elif (value < -60):
-            global_.window.ui.RSSI.setStyleSheet("background-color: yellow; color: black")
-            # styleChangeProperty(global_.window.ui.RSSI, 'color', 'black')
-            # styleChangeProperty(global_.window.ui.RSSI, 'background-color', 'yellow')
-        else:
-            global_.window.ui.RSSI.setStyleSheet("background-color: green; color: black")
-            # styleChangeProperty(global_.window.ui.RSSI, 'color', 'black')
-            # styleChangeProperty(global_.window.ui.RSSI, 'background-color', 'green')
+    rssi_value_sig = pyqtSignal(float)
 
     def __init__(self):
         QThread.__init__(self)
@@ -39,8 +17,9 @@ class WatcherThread(QThread):
         self.logger = get_logger('Watcher')
 
         if global_.window:
-            self.coordinate_value_sig.connect(global_.window.workWidget.ui.Coordinate.setValue)
             self.road_battery_sig.connect(global_.window.ui.battery_road.setValue)
+            self.road_battery_sig.connect(global_.window.road_battery_value_slot)
+            self.rssi_value_sig.connect(global_.window.rssi_value_slot)
 
         # Connect signals to slots.
         if global_.window.workWidget:
@@ -75,55 +54,26 @@ class WatcherThread(QThread):
 
             global_.mutex.tryLock(timeout=1)
 
-            if (int(t) % 5 == 0):
+            if int(t) % 5 == 0:
                 # Update progress bars.
-                self.rssi_value_slot(global_.mbeeThread.RSSI)
+                self.rssi_value_sig.emit(global_.mbeeThread.RSSI)
                 v = global_.roadData.voltage
                 self.road_battery_sig.emit(v)
-
-                # If motor is offed, display all elements in gray.
-                if global_.roadData.mode != -1:
-                    if v < 20:
-                        global_.window.ui.battery_road.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: red}')
-                        global_.window.workWidget.ui.Acceleration.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: red}')
-                        global_.window.workWidget.ui.Braking.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: red}')
-                        global_.window.workWidget.ui.Velocity.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: red}')
-                        global_.window.workWidget.ui.Coordinate.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: red}')
-                    else:
-                        global_.window.ui.battery_road.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: QColor (0,0,180)}')
-                        global_.window.workWidget.ui.Acceleration.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: QColor (0,0,180)}')
-                        global_.window.workWidget.ui.Braking.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: QColor (0,0,180)}')
-                        global_.window.workWidget.ui.Velocity.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: QColor (0,0,180)}')
-                        global_.window.workWidget.ui.Coordinate.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: QColor (0,0,180)}')
-                else:
-                    global_.window.ui.battery_road.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: gray}')
-                    global_.window.workWidget.ui.Acceleration.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: gray}')
-                    global_.window.workWidget.ui.Braking.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: gray}')
-                    global_.window.workWidget.ui.Velocity.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: gray}')
-                    global_.window.workWidget.ui.Coordinate.setStyleSheet('QProgressBar {text-align: center} QProgressBar::chunk {background-color: gray}')
-
-                # Show base numbers.
-                if global_.roadData.bases_init_swap:
-                    global_.window.workWidget.ui.Base1.setText('2')
-                    global_.window.workWidget.ui.Base2.setText('1')
-                else:
-                    global_.window.workWidget.ui.Base1.setText('1')
-                    global_.window.workWidget.ui.Base2.setText('2')
 
             global_.window.base1.emit(global_.roadData.base1_set)
             global_.window.base2.emit(global_.roadData.base2_set)
 
             # Show coordinate on progress bar.
-            if (global_.roadData.base1_set and global_.roadData.base2_set):
+            if global_.roadData.base1_set and global_.roadData.base2_set:
                 length = (global_.roadData.base2 - global_.roadData.base1)
                 value = (global_.roadData.coordinate - global_.roadData.base1) / length
                 if value > 1:
                     value = 1
                 if value < 0:
                     value = 0
-                self.coordinate_value_sig.emit(value * 100)
+                global_.window.coordinate_value_sig.emit(value * 100)
             else:
-                self.coordinate_value_sig.emit(0)
+                global_.window.coordinate_value_sig.emit(0)
             global_.mutex.unlock()
 
             time.sleep(0.05)
@@ -162,6 +112,7 @@ class ControlThread(QThread):
 
         self.changeMenu_sig.connect(global_.window.changeMenu)
 
+        self.t = 0
         self.menu_t = 0
         self.home_t = 0
         self.base_t = 0
@@ -200,10 +151,10 @@ class ControlThread(QThread):
             t = time.time()
 
             for i in range(1, 3+1):
-                encValue = self.controller.getEncoderValue(i)
-                self.controller.setIndicator(i, round(encValue/10))
+                enc_value = self.controller.getEncoderValue(i)
+                self.controller.setIndicator(i, round(enc_value/10))
 
-            if (self.t % 10):
+            if self.t % 10:
                 # TODO: add to screen
                 self.controller.getBatteryLevel()
 
